622f23e1385c47d4ac353b7e63f54a269332a8a1
TimNguyen21 <tim.nguyen2112@gmail.com>
2024-06-29 22:36:05 -0700

    [main] Updated useState for saving current number fact

--- src/App.js
+++ src/App.js
@@ -9,11 +9,14 @@ function App() {
   function getRequestInformation(number, numberType) {
     console.log(numberType);
     (number === '') ? console.log('random') : console.log(number);
+
+    setCurrentNumberFact({id: Date.now(), numberFact: numberType});
   }
 
   return (
     <div className="App">
       <NumberFactsForm getRequestInformation={getRequestInformation}/>
+      <input type='button' value='Check Number Results State' onClick={() => {console.log(currentNumberFact)}} />
     </div>
   );
 }
