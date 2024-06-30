fc126f8307e895f0e20d829ace3191ca5fab09bc
TimNguyen21 <tim.nguyen2112@gmail.com>
2024-06-29 23:28:23 -0700

    [main] Updated page layout for save number facts section

--- src/App.js
+++ src/App.js
@@ -16,18 +16,24 @@ function App() {
 
   async function fetchNumberFacts(APILink) {
     fetch(APILink)
-    .then(response => response.json())
-    .then(data => {setCurrentNumberFact({id: Date.now(), numberFact: data.text})})
-    .catch(err => {console.log(err)});
+      .then(response => response.json())
+      .then(data => {setCurrentNumberFact({id: Date.now(), numberFact: data.text})})
+      .catch(err => {console.log(err)});
   }
 
   return (
     <div className="App">
-      <NumberFactsForm getNumberFact={getNumberFact}/>
-      <br></br>
-      <br></br>
-      <br></br>
-      <label>{currentNumberFact['numberFact']}</label>
+      <section className='number-facts-form'>
+        <NumberFactsForm getNumberFact={getNumberFact}/>
+        <br></br>
+        <br></br>
+        <br></br>
+        <label>{currentNumberFact['numberFact']}</label>
+        <input type='button' value='Save Number Fact'/>
+      </section>
+      <section className='number-facts-saves'>
+        save results
+      </section>
     </div>
   );
 }
